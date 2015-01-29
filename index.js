8f0b3b10addfa2e386b256a264e6facd5ba5c3db
Jordan Harband <ljharb@gmail.com>
2015-01-29 10:48:38 -0800

    Use `is-callable` to remove Object#toString checks.

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 'use strict';
 
-var toStr = Object.prototype.toString;
+var isCallable = require('is-callable');
 var fnToStr = Function.prototype.toString;
 var isNonArrowFnRegex = /^\s*function/;
 var isArrowFnWithParensRegex = /^\([^\)]*\) *=>/;
 var isArrowFnWithoutParensRegex = /^[^=]*=>/;
 
 module.exports = function isArrowFunction(fn) {
-	if (typeof fn !== 'function') { return false; }
-	var typeStr = toStr.call(fn);
-	var fnStr = typeStr === '[object Function]' ? fnToStr.call(fn) : '';
+	if (!isCallable(fn)) { return false; }
+	var fnStr = fnToStr.call(fn);
 	return fnStr.length > 0
 		&& !isNonArrowFnRegex.test(fnStr)
 		&& (isArrowFnWithParensRegex.test(fnStr) || isArrowFnWithoutParensRegex.test(fnStr));
