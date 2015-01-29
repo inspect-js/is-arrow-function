c5e42e61ff6a30963c57a8cb81e187fab25d60df
Jordan Harband <ljharb@gmail.com>
2015-01-29 09:28:58 -0800

    Bail out early if typeof is not "function".
    Relates to #1.

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var isArrowFnWithParensRegex = /^\([^\)]*\) *=>/;
 var isArrowFnWithoutParensRegex = /^[^=]*=>/;
 
 module.exports = function isArrowFunction(fn) {
+	if (typeof fn !== 'function') { return false; }
 	var typeStr = toStr.call(fn);
 	var fnStr = typeStr === '[object Function]' ? fnToStr.call(fn) : '';
 	return fnStr.length > 0
