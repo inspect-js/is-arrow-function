1bdb005090ba7342fd559dcedc296c6200294915
Jordan Harband <ljharb@gmail.com>
2017-09-01 01:17:58 -0700

    [Dev Deps] update `eslint`, `@ljharb/eslint-config`, `jscs`, `nsp`, `tape`

--- index.js
+++ index.js
@@ -3,11 +3,13 @@
 var isCallable = require('is-callable');
 var fnToStr = Function.prototype.toString;
 var isNonArrowFnRegex = /^\s*function/;
-var isArrowFnWithParensRegex = /^\([^\)]*\) *=>/;
+var isArrowFnWithParensRegex = /^\([^)]*\) *=>/;
 var isArrowFnWithoutParensRegex = /^[^=]*=>/;
 
 module.exports = function isArrowFunction(fn) {
-	if (!isCallable(fn)) { return false; }
+	if (!isCallable(fn)) {
+		return false;
+	}
 	var fnStr = fnToStr.call(fn);
 	return fnStr.length > 0 &&
 		!isNonArrowFnRegex.test(fnStr) &&
